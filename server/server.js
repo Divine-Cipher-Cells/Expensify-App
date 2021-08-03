93fae0f2759fad35cfd54ec6ab9fd409f275357a
Divine-Cipher-Cells <DivineCipherCells@gmail.com>
2021-08-03 10:29:08 +0530

    heroku 3

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express=require("express");
 const path=require("path");
 const publicPath=path.join(__dirname,"../","public")
 const app=express();
-const port=Process.env.PORT || 3000;
+const port=process.env.PORT || 3000;
 
 app.use(express.static(publicPath));
 app.get("*",(req,res) =>{
